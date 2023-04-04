746214968c695c2b5bd847f9b17f00e39d9e338d
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-04-04 16:11:31 +0530

    hii

--- src/main/java/com/example/MongoDemo/Repository/EmployeeRepository.java
+++ src/main/java/com/example/MongoDemo/Repository/EmployeeRepository.java
@@ -7,12 +7,9 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
-    List<Employee> findByDepartment(String department);
-
-    //The below code will access the nested document
+    List<Employee> findByDepartment(String department);    //The below code will access the nested document
 //    @Query("{'address.city':{$in:[?0, ?1]}")
 //    List<Employee> getEmployeeByCity(String city0, String city1);
-
     @Query("{'address.city':{$in:[?0]}}")
     List<Employee> getEmployeeByCity(String city);
 }
