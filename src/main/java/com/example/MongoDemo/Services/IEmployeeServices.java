6439aa7df9d5c031641cfcc1bbb7e7662bf0cf62
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-04-04 13:30:50 +0530

    sadfs

--- src/main/java/com/example/MongoDemo/Services/IEmployeeServices.java
+++ src/main/java/com/example/MongoDemo/Services/IEmployeeServices.java
@@ -2,12 +2,18 @@ package com.example.MongoDemo.Services;
 
 import com.example.MongoDemo.Domain.Employee;
 import com.example.MongoDemo.Exception.EmployeeAlreadyExistException;
+import com.example.MongoDemo.Exception.EmployeeNotFoundExceptoin;
 
 import java.util.List;
 
 public interface IEmployeeServices {
     Employee addEmployee(Employee employee) throws EmployeeAlreadyExistException;
-//    List<Employee> findEmployee();
+    List<Employee> getEmployees();
 
+    Employee getEmpById(int id) throws EmployeeNotFoundExceptoin;
 
+    Employee updateEmployee(Employee employee, int id) throws EmployeeNotFoundExceptoin;
+    boolean deleteEmp(int id);
+    List<Employee> getEmpByDepartment(String deptName);
+    List<Employee> getEmpCity(String city);
 }
